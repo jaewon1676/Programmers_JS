4ff0ddd490255c000935f395ddd90c54fb79abfc
jaewon1676 <jaewon1676@daum.net>
2022-02-21 00:39:34 +0900

    음양 더하기

---
+++ Level_1/음양 더하기.js
@@ -0,0 +1,15 @@
+// https://programmers.co.kr/learn/courses/30/lessons/76501?language=javascript
+
+function solution(absolutes, signs) {
+    var answer = 0
+    for(var i=0; i<absolutes.length; i++){
+        if (signs[i] === false){
+            answer = answer - absolutes[i]
+        }
+        else{
+            answer = answer + absolutes[i]
+        }
+    }
+    
+    return answer;
+}

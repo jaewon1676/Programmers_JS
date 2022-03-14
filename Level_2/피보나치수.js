68f822bd726bf6edb1c528e9bec718bd1663e41d
jaewon <jaewon1676@daum.net>
2022-03-15 08:39:04 +0900

    피보나치수

---
+++ Level_2/피보나치수.js
@@ -0,0 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12945
+
+function solution(n) {
+    let f_1 = 0;
+    let f_2 = 1;
+    
+    for (let i=2; i<=n; i++) {
+        let temp = (f_1 + f_2) % 1234567;
+        f_1 = f_2;
+        f_2 = temp;
+    }
+    
+    return f_2;
+}

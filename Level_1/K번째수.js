be962b37c1a66394e67049828fe16689a5518a1c
jaewon <jaewon1676@daum.net>
2022-03-24 23:51:35 +0900

    K번째수

---
+++ Level_1/K번째수.js
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42748?language=javascript
+
+function solution(array, commands) {
+  var result = [];
+  var temp = [];
+  for(var i=0; i<commands.length; i++){
+      temp = array.slice(commands[i][0] - 1, commands[i][1]).sort((a,b) => {return a - b}) 
+      console.log(temp)
+      result.push(temp[commands[i][2]-1])
+  }
+  return result
+}

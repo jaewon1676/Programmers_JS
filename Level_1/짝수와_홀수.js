1ca212bf56fa84e6fcfcc848c99b2102779cd3a6
jaewon <jaewon1676@daum.net>
2022-04-03 21:51:19 +0900

    짝수와 홀수

--- Level_1/짝수와_홀수.js
+++ Level_1/짝수와_홀수.js
@@ -2,6 +2,7 @@
 
 function solution(num) {
   let answer
-  (num % 2 == 0 ? answer = 'Even' : answer = 'Odd')
+  (num % 2 == 0 ? answer = 'Even' : answer = 'Odd') 
+  // 삼항 연산자를 사용하여 참일경우 Even, 거짓일 경우 Odd를 반환.
   return answer;
 }

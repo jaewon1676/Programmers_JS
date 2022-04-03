edd7325d8aa0c3e91ea3115544b3fe32b8d9bdb1
jaewon <jaewon1676@daum.net>
2022-04-03 21:50:37 +0900

    짝수와 홀수

---
+++ Level_1/짝수와_홀수.js
@@ -0,0 +1,7 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12937?language=javascript
+
+function solution(num) {
+  let answer
+  (num % 2 == 0 ? answer = 'Even' : answer = 'Odd')
+  return answer;
+}

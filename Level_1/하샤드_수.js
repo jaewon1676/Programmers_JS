5709fae70a9fb4513d3afbf21e9d19e6f1d46b8f
jaewon <jaewon1676@daum.net>
2022-04-03 23:39:37 +0900

    하샤드 수

---
+++ Level_1/하샤드_수.js
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12947?language=javascript
+
+function solution(x) {
+  let sum = 0;
+  let arr = String(x).split(''); // 숫자를 하나씩 분리한다. 
+
+  for(var i=0; i<arr.length; i++) {
+      sum += Number(arr[i]); // 각 숫자를 더해준다.
+  }
+
+return (x % sum == 0) ? true : false; // 자릿수의 합으로 x가 나누어지면 하샤드 수
+}

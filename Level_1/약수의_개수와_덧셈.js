051eb708d3cc172beacbb94cc937efdd5c47ed6e
jaewon <jaewon1676@daum.net>
2022-04-04 13:35:35 +0900

    약수의 개수와 덧셈

---
+++ Level_1/약수의_개수와_덧셈.js
@@ -0,0 +1,15 @@
+// https://programmers.co.kr/learn/courses/30/lessons/77884?language=javascript#
+
+function solution(left, right) {
+  var answer = 0;
+  
+  for (left; left<=right; left++){
+      // left의 제곱근이 정수면 약수의 개수는 홀수
+      if (Number.isInteger(Math.sqrt(left))){
+          answer -= left 
+      } else {
+          answer += left
+      }
+  }
+  return answer;
+}

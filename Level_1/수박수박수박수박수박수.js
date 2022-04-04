07caeefc29075084752da679b39acd7ea0a0b874
jaewon <jaewon1676@daum.net>
2022-04-04 14:19:57 +0900

    수박수박수박수박수박수?

---
+++ Level_1/수박수박수박수박수박수.js
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12922?language=javascript
+
+function solution(n) {
+  let str = '';
+  for (let i=0; i<n; i++){
+      // 삼항 연산자와 +로 문자열을 붙여주어 추가.
+      (i % 2 == 0 ? str = str + '수' : str = str + '박')
+  }
+  return str;
+}

1326470579f33331c50b1defa43be3472de64e1d
jaewon <jaewon1676@daum.net>
2022-04-18 17:04:04 +0900

    최댓값과 최솟값

---
+++ Level_2/최댓값과-최솟값.js
@@ -0,0 +1,11 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12939?language=javascript
+
+function solution(s) {
+  s = s.split(' ')
+  s.sort((a, b) => {return a - b}) // 오름차순 정렬
+  
+  let small = s[0] // s의 최솟값
+  let large = s[s.length - 1] // s의 최댓값
+  
+  return (small + ' ' + large)
+}
